447d9b16fa3385f49a5669c0370f7f591370a732
Joker-04 <155981494+Joker-04@users.noreply.github.com>
2025-10-20 19:09:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,13 +54,13 @@ CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla
 
 
 # Verify
-VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
-DEENDAYAL_VERIFY_EXPIRE = int(environ.get('DEENDAYAL_VERIFY_EXPIRE', 2)) # Add time in hours
+VERIFY = bool(environ.get('VERIFY', True)) # Verification On ( True ) / Off ( False )
+DEENDAYAL_VERIFY_EXPIRE = int(environ.get('DEENDAYAL_VERIFY_EXPIRE', 8)) # Add time in hours
 DEENDAYAL_VERIFIED_LOG = int(environ.get('DEENDAYAL_VERIFIED_LOG', '-1002417525913')) #Log channel id ( make sure bot is admin )
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/how_to_downlod3/14') # How to open tutorial link for verification
 
 # Shortner 
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'arolinks.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '363401108da898a9de1f9a046fd8e6128896ce3b')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/how_to_downlod3/14') # Tutorial video link for opening shortlink website 
